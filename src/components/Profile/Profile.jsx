05a3c9cb8f0ce780c1f0eaef30e0cb31b9b6ec62
UMAtaullin <ataullinural@gmail.com>
2024-09-29 12:11:05 +0500

    feat: start Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import cls from './Profile.module.css'
 
 const Profile = () => {
   return (
-    <div className={cls.content}>
-      <div>Профиль пользователя</div>
-      <div>ava + description</div>
-      <div>
-        My posts
-        <div>New post</div>
-        <div className={cls.posts}>
-          <div className={cls.item}>post 1</div>
-          <div className='item'>post 2</div>
+    <div className='content'>
+      <div className='container'>
+        <h2>Профиль пользователя</h2>
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div className='posts'>
+            <div className='item'>post 1</div>
+            <div className='item'>post 2</div>
+          </div>
         </div>
       </div>
     </div>
