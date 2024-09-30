b2bba2e2e70179d07654bc3ae1664a14eda81899
UMAtaullin <ataullinural@gmail.com>
2024-09-30 20:11:32 +0500

    feat: division into subcomponents

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
+import cls from './Profile.module.css'
 
 const Profile = () => {
   return (
     <div className='content'>
       <div className='container'>
-        <h2>Профиль пользователя</h2>
-        <div>ava + description</div>
-        <div>
-          My posts
-          <div>New post</div>
-          <div className='posts'>
-            <div className='item'>post 1</div>
-            <div className='item'>post 2</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, blanditiis? Vitae quam sapiente ducimus perspiciatis vero inventore explicabo! Repellat ullam voluptate nam quia eligendi minus alias fuga provident officiis odit!</div>
+        <div className={cls.profile}>
+          <div className={cls.ava}>
+            <img
+              src='https://avatars.dzeninfra.ru/get-zen_doc/9759668/pub_645cec1a20e1c7242b32db52_6460ad5c6edf700dfdf05e3a/scale_1200'
+              alt=''
+            />
+          </div>
+          <div className={cls.description}>
+            <p className={cls.name}>Name: Ural Ataullin</p>
+            <p className={cls.name}>Date: 24.01.1988</p>
+            <p className={cls.name}>Telegram: @UMAtaullin</p>
+            <p className={cls.name}>Interests: programming</p>
           </div>
         </div>
-      </div>
+        <div className="posts">
+          My posts
+          <div>New post</div>
+          <div className='items'>
+            <div className='item'>post 1</div>
+            <div className='item'>post 2</div>
+            <div className='item'>post 3</div>
+          </div>
+        </div>
+        </div>
     </div>
   )
 }
