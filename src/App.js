f7e35d93f0ee1ddc11e8d207904d78e072ca2776
UMAtaullin <ataullinural@gmail.com>
2024-10-02 19:47:39 +0500

    feat: BrowserRouter Routes Route path element component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Messages from './components/Messages/Messages';
 import Profile from './components/Profile/Profile';
 import SideBar from './components/SideBar/SideBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <SideBar/>
-      {/* <Profile/> */}
-      <Messages />
+        <div className="main">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/messages" element={<Messages />} />
+          </Routes>
+        </div>
     </div>
   );
 }
