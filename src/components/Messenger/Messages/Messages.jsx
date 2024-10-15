697f56ac1e47f20e5141cb55318584f2280c159b
UMAtaullin <ataullinural@gmail.com>
2024-10-15 15:27:55 +0500

    feat: sent message for Messages

--- src/components/Messenger/Messages/Messages.jsx
+++ src/components/Messenger/Messages/Messages.jsx
@@ -1,13 +1,19 @@
-import Message from './Message';
+import Message from './Message/Message';
+import cls from './Messages.module.css';
 
 const Messages = (props) => {
-  let messages = props.messages.map(el => 
-    <Message name={el.name} text={el.message} />);
+  let messages = props.messages.map((el) => (
+    <Message name={el.name} text={el.message} />
+  ));
   return (
-    <div className="messages">
+    <div className={cls.messages}>
       {messages}
+      <div className={cls.row}>
+        <textarea />
+        <button>Send</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Messages;
