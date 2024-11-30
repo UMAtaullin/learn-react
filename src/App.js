0164a655700e7d19f3c19a5263756c3112c11b02
UMAtaullin <ataullinural@gmail.com>
2024-11-30 15:03:03 +0500

    feat: add Header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import MessagesContainer from './components/Messenger/Messages/MessagesContainer';
+import Header from './components/Header/Header';
 
 const App = (props) => {
   // debugger
   return (
     <div className="app">
+      <Header />
       <SideBar />
       <div className="main">
         <Routes>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+
+const Header = (props) => {
+  return (
+    <header className={style.header}>
+      <div className={style.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
